597a512652ce15792795f15f79de8cbdc0912e61
Alfian Ferdinan <t0pramen19@gmail.com>
2022-02-27 23:25:50 +0700

    Initial commit again
    importing some dataset, core file, and example exports. Also core file code was altered for new folder structure purpose.

---
+++ Engine/core.py
@@ -0,0 +1,47 @@
+import csv
+import matplotlib.pyplot as plt
+import matplotlib.patches as patch
+from PIL import Image
+imgpath = r"../Raw/Raw_KdPengawas_Part1.png"
+csvpath = r"../Datas/patchdataset.csv"
+plt.rcParams["figure.figsize"] = [6.4, 4.8]
+plt.rcParams["figure.autolayout"] = True
+img = Image.open(imgpath)
+
+fig,ax = plt.subplots()
+
+# draw frames config.
+ax.axis('on')
+
+# show main image
+ax.imshow(img)
+
+# draw patches, automated with csv
+# placeholder hardcoded path
+with open(csvpath) as file:
+	patchSet = csv.DictReader(file)
+
+	for x in patchSet:
+		ax.add_patch(patch.Rectangle((float(x['coordX']), float(x['coord-Y'])), float(x['expandX']), float(x['expand-Y']), facecolor=x['status'], alpha=0.5))
+
+## axis tick config.
+## breakdown the limits of two axes and get the value
+#_, x_max = plt.gca().get_xlim()
+#y_min, _ = plt.gca().get_ylim()
+## define max ranges
+#x = np.random.randint(low=0, high=50, size=int(x_max))
+#y = np.random.randint(low=0, high=50, size=int(y_min))
+## set ranges between axis ticks
+#ax.set_xticks(np.arange(0, len(x)+1, 25))
+#ax.set_yticks(np.arange(0, len(y)+1, 25))
+
+
+# rotate x axis text for readability
+plt.xticks(rotation=45, ha='right')
+# show plot result
+plt.show()
+
+# save plot result
+#ax.axis('off')
+# plt.axis('off')
+# plt.savefig('../Exports/testexample_FigurePlot.pdf', format='pdf', dpi=600, bbox_inches='tight')
